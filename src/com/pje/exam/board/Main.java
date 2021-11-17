bb9d0856055eb88f6ad8339bf51f8fb6449cfca8
fmfu51 <fmfu5129@gmail.com>
2021-11-17 17:39:58 +0900

    제목, 내용 입력

--- src/com/pje/exam/board/Main.java
+++ src/com/pje/exam/board/Main.java
@@ -14,13 +14,23 @@ public class Main {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")){
+      if (cmd.equals("exit")) {
         break;
-      }
+      } else if (cmd.equals("/usr/article/write")) {
+        System.out.println("- 게시물 등록 -");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
 
-      System.out.printf("입력된 명령어 : %s\n", cmd);
+        int id = 1;
+        System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
+
+      } else {
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+      }
     }
-    System.out.println("== 프로그램 종류 ==");
+    System.out.println("== 프로그램 종료 ==");
     sc.close();
   }
 }
