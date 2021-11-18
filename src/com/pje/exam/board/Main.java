c96c8f74a415503c248274665ff5cc58d3b99907
unknown <fmfu5129@gmail.com>
2021-11-18 10:35:11 +0900

    최근 게시물 정보 노출

--- src/com/pje/exam/board/Main.java
+++ src/com/pje/exam/board/Main.java
@@ -1,7 +1,11 @@
 package com.pje.exam.board;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
+
+/*
+-  [ . ] /usr/article/detail 입력처리
+-  [ . ] /usr/article/detail 입력되면 가장 최근 게시물 정보 노출
+ */
 
 public class Main {
 
@@ -12,6 +16,7 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
 
     int articlesLastId = 0;
+    Article lastArticle = null;
 
     while (true) {
       System.out.printf("명령) ");
@@ -19,6 +24,20 @@ public class Main {
 
       if (cmd.equals("exit")) {
         break;
+      } else if (cmd.equals("/usr/article/detail")) {
+
+        if (lastArticle == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = lastArticle;
+
+        System.out.println("- 게시물 상세내용 -");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %d\n", article.title);
+        System.out.printf("내용 : %d\n", article.body);
+
       } else if (cmd.equals("/usr/article/write")) {
         System.out.println("- 게시물 등록 -");
         System.out.printf("제목 : ");
@@ -30,6 +49,7 @@ public class Main {
         articlesLastId = id;
 
         Article article = new Article(id, title, body);
+        lastArticle = article;
         System.out.println("생성된 게시물 객체 : " + article);
 
         System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
@@ -37,18 +57,21 @@ public class Main {
       } else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
+
+      System.out.println("== 프로그램 종료 ==");
+      sc.close();
     }
-    System.out.println("== 프로그램 종료 ==");
-    sc.close();
   }
 }
+
 
 class Article {
   int id;
   String title;
   String body;
 
-  Article(int id, String title, String body){
+
+  Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
@@ -59,4 +82,5 @@ class Article {
     return String.format("{id: %d, title: \"%s\"}", id, title);
 
   }
+
 }
